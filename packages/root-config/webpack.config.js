4482d21898ea3ffbdc56d00f3e2d94f88b4ada47
luohuidong <luohuidong01@126.com>
2021-08-22 14:50:59 +0800

    refactor: 通过本地加载脚本的方式替换CDN引入的方式，适用于内网无法访问外网的情况

--- packages/root-config/webpack.config.js
+++ packages/root-config/webpack.config.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = "vue-admin";
@@ -12,11 +13,14 @@ module.exports = (webpackConfigEnv, argv) => {
     disableHtmlGeneration: true,
   });
 
+  console.log(path.resolve(__dirname, "publicPath"));
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
     plugins: [
       new HtmlWebpackPlugin({
+        title: "single-spa-admin",
         inject: false,
+        publicPath: path.resolve(__dirname, "public"),
         template: "src/index.ejs",
         templateParameters: {
           isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
