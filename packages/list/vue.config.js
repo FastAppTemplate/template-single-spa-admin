9c1e85daca64ed31d4740dd83d40528db0a7e3f9
luohuidong <luohuidong01@126.com>
2021-09-06 01:03:10 +0800

    build: 每一个子应用的 css class 添加指定的前缀

--- packages/list/vue.config.js
+++ packages/list/vue.config.js
@@ -15,7 +15,7 @@ module.exports = {
         // For Vue CLI v3 users, please refer to css-loader v1 documentations
         // https://github.com/webpack-contrib/css-loader/tree/v1.0.1
         modules: {
-          localIdentName: "header-[name]-[hash]",
+          localIdentName: "list-[name]-[hash]",
         },
       },
     },
